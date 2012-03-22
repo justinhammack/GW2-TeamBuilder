0d356446a8032f11fe0f4e771be1f7ed02bff809
Justin Hammack <info@justinhammack.com>
2012-03-21 21:22:20 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GW2Teambuilder::Application.config.secret_token = '1d8a7cbc3a6244fa05bb2f5f3c5f04566657f2fb1c1e5a5848ffd3f4d0023de1287cb89ccf0fad3c6522a4113fb6588a6ca11690c2066ec80ab442bab5e254b4'
